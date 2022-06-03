96e35d3d87c1c97fb61239604b5a72f0a7059984
Rao Shahbaz <raoshahbaz81@gmail.com>
2022-06-03 19:17:17 +0500

    example env

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express');
+require('dotenv').config();
+const express = require('serverless-express/express');
 const { default: mongoose } = require('mongoose');
-const todoRoute = require('./Routes/todoroute')
+const todoRoute = require('./Routes/todoroute');
+const serverless = require('serverless-express/handler');
 
 const app = express();
 
 // app.get('/', (req, res)=> {
 //     res.send('hello world');
 // });
-mongoose.connect('mongodb://localhost:27017/todoDB');
+console.log(process.env.MONGO_URI, "fghjk")
+mongoose.connect(process.env.MONGO_URI);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
@@ -19,3 +22,6 @@ app.use('/api/', todoRoute);
 app.listen(7000,  () => {
     console.log('Example app listening on port 7000!')
   });
+
+//exports.http = serverless(app);
+
